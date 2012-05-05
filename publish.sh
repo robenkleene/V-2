88f87902aab5d46791ff78edfbe1f9dd15eba735
Roben Kleene <robenk@gmail.com>
2012-05-05 15:57:30 -0400

    Removed old files

--- publish.sh
+++
@@ -1,4 +0,0 @@
-cp ./css/Pynchon.css ~/Library/Application\ Support/Marked/Custom\ CSS/
-cp ./css/Pynchon.css ~/Library/Application\ Support/BBEdit/Preview\ CSS/
-cp ./css/Pynchon.css ./Pynchon.nnwstyle/
-cp -R ./Pynchon.nnwstyle ~/Library/Application\ Support/NetNewsWire/StyleSheets/
