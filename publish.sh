ac166fb69acd7889c90cf2c7679b2b265d9356b6
Roben Kleene <robenk@gmail.com>
2012-05-04 22:56:25 -0400

    Finished version 1.0

--- publish.sh
+++ publish.sh
@@ -1,1 +1,3 @@
 cp ./css/Pynchon.css ~/Library/Application\ Support/Marked/Custom\ CSS/
+cp ./css/Pynchon.css ./Pynchon.nnwstyle/
+cp -R ./Pynchon.nnwstyle ~/Library/Application\ Support/NetNewsWire/StyleSheets/
